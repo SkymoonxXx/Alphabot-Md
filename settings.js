b308b049c19ae8877d54b1252d42cecd4b81746a
SkymoonxXx <107530677+SkymoonxXx@users.noreply.github.com>
2022-06-22 13:12:14 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,21 +18,21 @@ global.autoread = false // auto read pesan / message
 //global.available = false //status online (online)
 
 // Other
-global.botname = "Xavier-Botz" //namabot kalian
-global.ownername= "Xavier" //nama kalian
+global.botname = "Zuhri-Botz" //namabot kalian
+global.ownername= "Zuhri" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
 global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "xavierbotz@gmail.com" //bebas
+global.email = "Zuhribotz@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.ownernomer = "6283862006229" // nomor wa kalian
 global.ownernomerr = "+6283862006229" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["6283862006229","6283862006229","6283862006229"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Xavier-Botz' //sticker wm ubah
-global.author = 'Di Buat Oleh Xavier' //sticker wm ganti nama kalian
+global.owner = ["6282173459644","6282173459644","6282173459644"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© Zuhri-Botz' //sticker wm ubah
+global.author = 'Di Buat Oleh Zuhri' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
